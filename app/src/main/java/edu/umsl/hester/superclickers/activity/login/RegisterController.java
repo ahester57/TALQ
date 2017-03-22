b632d1a85747213bdb63848693ec1510e782daac
Austin Hester <ahester57@gmail.com>
2017-03-21 19:20:52 -0500

    outlined project structure moreso

---
+++ app/src/main/java/edu/umsl/hester/superclickers/activity/login/RegisterController.java
@@ -0,0 +1,8 @@
+package edu.umsl.hester.superclickers.activity.login;
+
+/**
+ * Created by Austin on 3/21/2017.
+ */
+
+public class RegisterController {
+}
---
+++ app/src/main/java/edu/umsl/hester/superclickers/activity/quiz/QuizModel.java
@@ -0,0 +1,8 @@
+package edu.umsl.hester.superclickers.activity.quiz;
+
+/**
+ * Created by Austin on 3/21/2017.
+ */
+
+public class QuizModel {
+}
